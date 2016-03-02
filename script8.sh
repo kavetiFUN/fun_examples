c2a96aa66cfd3c14c118e5725d9f7cd358cef94a
sridhar-newsdistill <sridhar.newsdistill@gmail.com>
2016-03-02 17:12:36 +0530

    case demo is added

--- script8.sh
+++ script8.sh
@@ -1,1 +1,15 @@
-echo "case demos"
+echo "enter the os you like"
+y="is good"
+read x
+case $x in
+mac)
+echo -n
+echo "so you like mac $x $y"
+;;
+win)
+echo "nobody likes windows man $x $y"
+;;
+linux)
+echo "ultimate os to consider $x $y"
+;;
+esac
